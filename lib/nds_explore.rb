cba8d2231628d954c02d3adb5197c2c737bd12ee
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-11 01:00:28 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,6 +9,9 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  
-  
+  movie = 0 
+  while movie < directors_database[0][:movies].length do 
+    puts directors_database[0][:movies][movie][:title]
+    movie += 1 
+  end 
 end
